0eec09cf29b22772f7db2bcf1e6ea7ed94b1a849
Xin Zhang <xinzhang@Mikes-iMac.local>
2022-04-18 20:23:35 -0700

    Update main.go

--- main/main.go
+++ main/main.go
@@ -1,6 +1,16 @@
 package main
 
+import "time"
+
 func main() {
-	client := &Client{URL: "google.com:80"}
-	client.Do()
+	server := &Server{}
+	go func() {
+		server.Do()
+	}()
+	client := &Client{URL: ":1200"}
+	for i := 0; i < 100; i++ {
+		client.Do()
+	}
+	server.Done <- struct{}{}
+	time.Sleep(5 * time.Second)
 }
