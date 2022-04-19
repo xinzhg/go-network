0b6d93701831deaa6d927e1fc059924fc37bee2a
Xin Zhang <xinzhang@Mikes-iMac.local>
2022-04-18 20:09:46 -0700

    Add server.go

---
+++ main/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+)
+
+type Server struct {
+}
+
+func (s *Server) Do() {
+	service := ":1200"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(tcpAddr.String())
+	listener, err := net.Listen("tcp", tcpAddr.String())
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("error:", err)
+			continue
+		}
+		daytime := time.Now().String()
+		cnt, err := conn.Write([]byte(daytime))
+		if err != nil {
+			log.Println("error", err)
+			continue
+		}
+		log.Println("cnt:", cnt)
+		conn.Close()
+	}
+}
