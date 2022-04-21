54af9170d60c3d1b025eb1de02a700ea6503d800
Xin Zhang <xinzhang@Mikes-iMac.local>
2022-04-21 14:25:29 -0700

    Debug code

--- main/server.go
+++ main/server.go
@@ -49,38 +49,38 @@ func (s *Server) Do() {
 			log.Println(SERVER + "terminating server")
 			return
 		default:
-			go func() {
-				//conn.SetDeadline(time.Now().Add(3 * time.Second))
-				//defer conn.Close()
-				daytime := time.Now().String() + EOF
-				recv := [512]byte{}
-				cnt, err := conn.Read(recv[:])
-				if err != nil {
-					log.Println(SERVER+"error", err)
-					//conn.Close()
-					return
-				}
-				fmt.Println(SERVER, "what server recev:", string(recv[:]), len(recv[:]), len(recv))
-				log.Println(SERVER+"cnt in read server", cnt)
-				//conn.SetNoDelay(true)
-				log.Println(SERVER, "before io.copy")
-				//io.Copy(ioutil.Discard, conn)
-				log.Println(SERVER, "after io.copy")
-				log.Println(SERVER, "before write")
-				log.Println(SERVER, "before write")
-				//fd, err := conn.File()
-				//cnt, err = fd.Write([]byte(daytime))
-				//err = fd.Sync()
-				cnt, err = conn.Write([]byte(daytime))
-				log.Println(SERVER, "after write")
-				if err != nil {
-					log.Println(SERVER+"error", err)
-					//conn.Close()
-					return
-				}
+			//go func() {
+			//conn.SetDeadline(time.Now().Add(3 * time.Second))
+			//defer conn.Close()
+			daytime := time.Now().String() + EOF
+			recv := [512]byte{}
+			cnt, err := conn.Read(recv[:])
+			if err != nil {
+				log.Println(SERVER+"error", err)
 				//conn.Close()
-				log.Println(SERVER+"cnt in server:", cnt)
-			}()
+				return
+			}
+			fmt.Println(SERVER, "what server recev:", string(recv[:]), len(recv[:]), len(recv))
+			log.Println(SERVER+"cnt in read server", cnt)
+			//conn.SetNoDelay(true)
+			log.Println(SERVER, "before io.copy")
+			//io.Copy(ioutil.Discard, conn)
+			log.Println(SERVER, "after io.copy")
+			log.Println(SERVER, "before write")
+			log.Println(SERVER, "before write")
+			//fd, err := conn.File()
+			//cnt, err = fd.Write([]byte(daytime))
+			//err = fd.Sync()
+			cnt, err = conn.Write([]byte(daytime))
+			log.Println(SERVER, "after write")
+			if err != nil {
+				log.Println(SERVER+"error", err)
+				//conn.Close()
+				return
+			}
+			//conn.Close()
+			log.Println(SERVER+"cnt in server:", cnt)
+			//}()
 		}
 	}
 }
