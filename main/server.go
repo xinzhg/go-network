d5a8f3c02998ae2e84019ac7e0b1c8df8764df41
Xin Zhang <xinzhang@Mikes-iMac.local>
2022-04-27 22:49:05 -0700

    Add code for supporting more fds

--- main/server.go
+++ main/server.go
@@ -38,26 +38,31 @@ func (s *Server) Do() {
 		panic(err)
 	}
 	s.listener = listener
-	conn, err := listener.AcceptTCP()
-	f, _ := conn.File()
-
-	epfd, err := u.EpollCreate1(0)
+	epfd, err := u.EpollCreate1(u.EPOLL_CLOEXEC)
 	if err != nil {
 		panic("error in creating epoll instance" + err.Error())
 	}
-	var event u.EpollEvent
-	event.Events = syscall.EPOLLIN | syscall.EPOLLOUT
-	event.Fd = int32(f.Fd())
-	err = u.EpollCtl(epfd, u.EPOLL_CTL_ADD, int(event.Fd), &event)
-	if err != nil {
-		panic("error in epoll ctl" + err.Error())
-	}
 
-	fmt.Println(SERVER, "conn's fd:", f.Fd())
-	if err != nil {
-		log.Println(SERVER, "error:", err)
-		return
-	}
+	go func() {
+		for {
+			conn, err := listener.AcceptTCP()
+			f, _ := conn.File()
+			var event u.EpollEvent
+			event.Events = syscall.EPOLLIN | syscall.EPOLLOUT
+			event.Fd = int32(f.Fd())
+			err = u.EpollCtl(epfd, u.EPOLL_CTL_ADD, int(event.Fd), &event)
+			if err != nil {
+				panic("error in epoll ctl" + err.Error())
+			}
+
+			fmt.Println(SERVER, "conn's fd:", f.Fd())
+			if err != nil {
+				log.Println(SERVER, "error:", err)
+				return
+			}
+		}
+	}()
+
 	var events [512]u.EpollEvent
 	for {
 		log.Println(SERVER, "looping")
@@ -71,10 +76,12 @@ func (s *Server) Do() {
 				panic("error in waiting" + err.Error())
 			}
 			log.Println(SERVER, "number of events from epoll", nevents)
+			efd := 0
 			for i := 0; i < nevents; i++ {
 				event := events[i]
 				bs := [512]byte{}
 				n, err := u.Read(int(event.Fd), bs[:])
+				efd = int(event.Fd)
 				if err != nil {
 					panic("error in epoll reading" + err.Error())
 				}
@@ -82,7 +89,7 @@ func (s *Server) Do() {
 			}
 			//go func() {
 			daytime := time.Now().String() + EOF
-			n, err := u.Write(int(event.Fd), []byte(daytime))
+			n, err := u.Write(int(efd), []byte(daytime))
 			if err != nil {
 				panic("error in epoll sending" + err.Error())
 			}
