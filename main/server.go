952e090ba43198ddd673af715095265f7a193ea6
Xin Zhang <xinzhang@Mikes-iMac.local>
2022-04-30 17:13:54 -0700

    Add code for debug

--- main/server.go
+++ main/server.go
@@ -114,7 +114,7 @@ func (s *Server) Do() {
 				m.Lock()
 				cnt++
 				m.Unlock()
-				log.Println(SERVER, "details:", "read size:", n, "type:", event.Events, "fd:", event.Fd, "pad", event.Pad, "cnt:", cnt)
+				log.Println(SERVER, "details:", string(bs[:]), "read size:", n, "type:", event.Events, "fd:", event.Fd, "pad", event.Pad, "cnt:", cnt)
 			}
 			//go func() {
 			daytime := time.Now().String() + EOF
